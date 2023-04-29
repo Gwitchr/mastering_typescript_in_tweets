943a7b60c6747d18d5d9c9259e2a7948210d53e1
Luis Casillas <casillasljaime@gmail.com>
2023-04-28 18:07:12 -0600

    examples of decorators and experimental metadata added

--- decorators/parameter_decorators.ts
+++ decorators/parameter_decorators.ts
@@ -1,9 +1,19 @@
 function decoradorParametro(
-	target: any,
-	methodName: string,
-	parameterIndex: number
+  target: any,
+  methodName: string,
+  parameterIndex: number
 ) {
-	console.log(`objetivo:${target.constructor} ⚠️`);
-	console.log(`nombre del método:${target[methodName]} ⚠️`);
-	console.log(`índice del parámetro:${parameterIndex} ⚠️`);
+  console.log(`objetivo:${target.constructor} ⚠️`);
+  console.log(`nombre del método:${target[methodName]} ⚠️`);
+  console.log(`índice del parámetro:${parameterIndex} ⚠️`);
 }
+
+class ClaseConDecoradorParametro {
+  print(@decoradorParametro impresion: string) {}
+}
+
+// objetivo:class ClaseConDecoradorParametro {
+// 	print(impresion) { }
+// } ⚠️
+// nombre del método:print(impresion) { } ⚠️
+// índice del parámetro:0 ⚠️
